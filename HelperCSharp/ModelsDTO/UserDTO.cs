9e972d0621dfae7eb7e83c046873c11f49426643
alexeiguriev <alexeiguriev1@gmail.comm>
2021-08-16 22:17:24 +0300

    Put DTOs in helper

---
+++ HelperCSharp/ModelsDTO/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Helper.ModelsDTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
